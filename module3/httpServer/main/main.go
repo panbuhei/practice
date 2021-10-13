020e1384a432306047b804ad655f9e22960589b5
“luoahong8” <luoahong@163.com>
2021-10-13 16:28:30 +0800

    readme

---
+++ module3/httpServer/main/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/healthz", healthz)
+	http.HandleFunc("/header", modifyResHeader)
+
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 1.接收客户端 request，并将 request 中带的 header 写入 response header
+func modifyResHeader(w http.ResponseWriter, r *http.Request) {
+	for i, v := range r.Header {
+		for _, v2 := range v {
+			w.Header().Add(i, v2)
+		}
+	}
+
+	// 2.读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+	version := os.Getenv("VERSION")
+	w.Header().Add("Version", version)
+
+	resHeader, err := json.Marshal(w.Header())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = w.Write(resHeader)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 4.当访问 localhost/healthz 时，应返回 200
+func healthz(w http.ResponseWriter, r *http.Request) {
+	if r.Host == "localhost" || r.Host == "127.0.0.1" {
+		_, err := w.Write([]byte("200"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
